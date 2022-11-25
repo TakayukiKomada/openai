ed46e09b9d36d0edc26c1a59e615873d87afea1a
TakayukiKomada <Takakoma.79@gmail.com>
2022-11-25 09:28:03 +0900

    ugoku

--- app.py
+++ app.py
@@ -1,22 +1,103 @@
 import os
-
+import pprint
+import time
+import numpy as np
+import urllib.error
+import urllib.request
+from PIL import Image
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from time import sleep
+import random
+import cv2
+
 
 app = Flask(__name__)
+
+x_list = []
+y_list = []
 
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.Model.list()
 
 
-@app.route("/", methods=("GET", "POST"))
+@app.route("/")
 def index():
+    return render_template("index.html")
+
+
+@app.route("/update", methods=("GET", "POST"))
+def update():
     if request.method == "POST":
         animal = request.form["animal"]
-        response = openai.Image.create(prompt=animal, n=2, size="1024x1024")
+        response = openai.Image.create(prompt=animal, n=1, size="1024x1024")
         image_url = response["data"][0]["url"]
-        return redirect(image_url)
+        with urllib.request.urlopen(image_url) as web_file:
+            image = Image.open(web_file)
+            sleep(10)
+            animal2 = random.sample(range(10000), 1)
+            image.save(f"static/image/imagesout_color_{animal}{animal2}.png")
+            # web_file.save(static/images/imagesout_color.png)
+
+        return redirect(url_for("update", result=image_url))
 
     result = request.args.get("result")
-    return render_template("index.html", result=result)
+    return render_template("update.html", result=result)
+
+
+# # 出力先のパスを生成
+# @app.route("/edit", methods=("GET", "POST"))
+# def onMouse(event, x, y, flags, params):
+#     if len(x_list) == 2:
+#         event = ""
+#     elif event == cv2.EVENT_LBUTTONDOWN:
+#         x_list.append(x)
+#         y_list.append(y)
+
+#     img = cv2.imread("static/image/man.png")
+
+#     # 画像を表示させる
+#     cv2.imshow("face", img)
+
+#     # クリックした際にonMouse関数が発火
+#     cv2.setMouseCallback("face", img)
+#     # bgr -> rgb
+#     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     # マスク画像の生成（マスク画像をかけたい画像サイズと同じ）
+#     mask = np.zeros(rgb.shape, dtype=np.uint8)
+
+#     cv2.rectangle(
+#         mask,
+#         (x_list[0], y_list[0]),
+#         (x_list[1], y_list[1]),
+#         (255, 255, 255),
+#         -1,
+#     )
+    # 画像の合成
+    rgb_and = cv2.bitwise_and(rgb, mask)
+    # 画像の表示
+    imgShow(rgb_and)
+    # waitKey(0)で画像上で何かしらのキーを押せば閉じられるようにする
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    # openai.Image.create_edit(
+    #     image1=open('static/image/man.png', "rb"),
+    #     mask=open("mask.png", "rb"),
+    #     prompt="A cute baby sea otter wearing a beret",
+    #     n=1,
+    #     size="1024x1024"
+    # )
+    # image_url2 = response["data"][0]["url"]
+    # with urllib.request.urlopen(image_url2) as web_file:
+    #         image = Image.open(web_file)
+    #         sleep(10)
+    #         animal2 = random.sample(range(10000), 1)
+    #         image.save(f"static/image/imagesout_color_{animal2}.png")
+
+    # result = request.args.get("result")
+    # return render_template("edit.html", result=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ check_coordinate.py
@@ -0,0 +1,88 @@
+import cv2
+import openai
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+x_list = []
+y_list = []
+
+
+def imgShow(imgList, row_num=1):
+    img_num = len(imgList)
+    # Matplotlibでグラフを描く際の基本パーツとしてFigureとAxesがあります。
+
+    # Figure：描画領域全体
+    # Axes：一つ一つのプロットを描く領域（座標軸）
+    if img_num == 1:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.imshow(imgList[0])
+
+    else:
+        fig, axes = plt.subplots(row_num, round(img_num / row_num))
+        ax = axes.ravel()
+        for i in range(img_num):
+            ax[i].imshow(imgList[i])
+            ax[i].set_xticks([])
+            ax[i].set_yticks([])
+
+    plt.show()
+
+
+img = cv2.imread("static/image/man.png")
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# マスク画像の生成（マスク画像をかけたい画像サイズと同じ）
+mask = np.zeros(rgb.shape, dtype=np.uint8)
+cv2.rectangle(
+    rgb,
+    (255, 126),
+    (377, 296),
+    (255, 255, 255),
+    -1,
+)
+imgShow([rgb])
+cv2.imwrite("static/image/rgb.png", rgb)
+url = "static/image/rgb.png"
+response = openai.Image.create_edit(
+    image=open("static/image/man.png", "rb"),
+    mask=open(url, "rb"),
+    prompt="A sunlit indoor lounge area with a pool containing a flamingo",
+    n=1,
+    size="1024x1024",
+)
+image_url = response["data"][0]["url"]
+
+
+# def onMouse(event, x, y, flags, params):
+#     if len(x_list) == 2:
+#         event = ""
+#         # bgr -> rgb
+#         rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#         # マスク画像の生成（マスク画像をかけたい画像サイズと同じ）
+#         mask = np.zeros(rgb.shape, dtype=np.uint8)
+#         cv2.rectangle(
+#             rgb,
+#             (x_list[0], y_list[0]),
+#             (x_list[1], y_list[1]),
+#             (255, 255, 255),
+#             -1,
+#         )
+#         # 画像の合成
+#         rgb_and = cv2.bitwise_and(rgb, mask)
+#         # 画像の表示
+#         imgShow(rgb_and)
+#     # waitKey(0)で画像上で何かしらのキーを押せば閉じられるようにする
+#     elif event == cv2.EVENT_LBUTTONDOWN:
+#         x_list.append(x)
+#         y_list.append(y)
+#         print(x_list, y_list)
+
+
+# # 画像を表示させる
+# cv2.imshow("face", img)
+# # クリックした際にonMouse関数が発火
+# cv2.setMouseCallback("face", onMouse)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
